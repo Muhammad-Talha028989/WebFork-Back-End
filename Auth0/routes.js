5492ebc169f8fdbf149691984b9212c337ee263b
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-07-17 12:58:12 +0500

    Update routes.js

--- Auth0/routes.js
+++ Auth0/routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const auth0Router = express.Router();
 
 auth0Router.get("/", (req, res) => {
-  res?.send(req?.oidc?.isAuthenticated());
+  res?.send(req?.oidc?.user);
 });
 
 module.exports = auth0Router;
